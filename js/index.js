2d0c1f7981b331640102623f00e59fa0e20690a1
DenisasJersovas <denisas.jersovas@gmail.com>
2017-09-18 18:21:03 +0300

    added v2 requirements for todos app

--- js/index.js
+++ js/index.js
@@ -1,28 +1,29 @@
-// it should have a function store todos
-// it should have a function display todos +
-// it should have a function add todos +
-// it should have a function delete todos
-// it should have a function edit todos
+// it should have a function store todos array on object
+// it should have a displayTodos method
+// it should have a addTodo method
+// it should have a changeTodo method
+// it should have a deleteTodo method
 
 
-// v1
+// v2
 
 var todos = [];
 
+// function to display todos
 function displayTodos(){
     console.log('My todos: ', todos);
 }
-
+// function to add new todo item
 function addTodo(newTodo){
     todos.push(newTodo);
     displayTodos();
 }
-
+// function to change sertain todo item
 function changeTodo(position, newValue){
     todos[position] = newValue;
     displayTodos();
 }
-
+// function to delete todo item
 function deleteTodo(position){
     todos.splice(position, 1);
     displayTodos();
